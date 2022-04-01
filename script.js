4adadbee07db19cba67c55737a21d9e2167e4ac0
AidenSPS <aidenkusmierczak2016@gmail.com>
2022-04-01 10:24:30 -0400

    askQuestion() is done, working on checkAnswer()
    JS: Working on checkAnswer, X and Y are still being defined as 0.
    
    CSS: Input and Button are not centered.

--- script.js
+++ script.js
@@ -12,5 +12,50 @@ interface.appendChild(title);
 //Code to the questionBox
 var questionBox = document.createElement("p");
 questionBox.setAttribute('id','questionBoxP');
-questionBox.innerHTML = "Testing"; //x+" * "+y+" = ?" is the proper string, insert when done
+questionBox.innerHTML = askQuestion(); //x+" * "+y+" = ?" is the proper string, insert when done
 interface.appendChild(questionBox);
+
+//Code for the inputBox
+var userInput = document.createElement("input");
+userInput.setAttribute("id","userAnswer");
+interface.appendChild(userInput); //Center the inputBox
+
+//Code for the Submit Button 
+var submitButton = document.createElement("button");
+submitButton.setAttribute("id","submit");
+submitButton.setAttribute("onClick","checkAnswer()");
+submitButton.innerText = "Submit";
+interface.appendChild(submitButton);
+
+
+//Globals needed for functions below
+var x = 0;
+var y = 0;
+var product = 0;
+var min = 3;
+var max = 10;
+
+
+//Coding the code for the string
+function askQuestion(){
+	x = Math.floor(Math.random()*10)+3;
+	y = Math.floor(Math.random()*10)+3;
+	product = (x * y);
+	let question = x+" X "+y+" = ?";
+	return question;
+}
+
+
+//Coding the correct or incorrect answer
+function checkAnswer(){
+	let userAnswer = document.getElementById("userAnswer");
+	let answer = userAnswer.value;
+	if(answer == product){
+		alert("Correct, "+x+" X "+y+" is "+product);
+		document.location = 'index.html';
+	}
+	else{
+		alert("Incorrect! "+x+" X "+y+" is "+product);
+		document.location = 'index.html';
+	}
+}
